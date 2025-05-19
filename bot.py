270bf3d283cbd50986a89574ce3c14e0af8dc9c7
Harshit07s <harshitnarang624@gmail.com>
2025-05-19 19:31:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from config import *
 
 
 name ="""
- BY CODEFLIX BOTS
+ культурным союзом 
 """
 
 
@@ -46,12 +46,12 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeflixSupport")
-        self.LOGGER(__name__).info(f"""BOT DEPLOYED BY @CODEFLIX_BOTS""")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Cultured_backup")
+        self.LOGGER(__name__).info(f"""BOT DEPLOYED BY @Cultured_networks""")
 
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Made by @Codeflix_Bots")   
+        self.LOGGER(__name__).info(f"Bot Running..! ")   
 
         # Start Web Server
         app = web.AppRunner(await web_server())
@@ -59,7 +59,7 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by owner</blockquote></b>")
         except: pass
 
     async def stop(self, *args):
@@ -70,7 +70,7 @@ class Bot(Client):
         """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+        self.LOGGER(__name__).info("Bot is now running. Thanks to jesus 🤡🙏🏻")
         try:
             loop.run_forever()
         except KeyboardInterrupt:
